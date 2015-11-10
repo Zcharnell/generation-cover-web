1caab521065f3e75956fcd11e4816aa930a53bd9
zach char <zcharnell@gmail.com>
2015-11-09 21:44:20 -0700

    server.js fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
   methodOverride = require('method-override');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/prod'));
+app.use(express.static(__dirname + '/dev'));
 
 server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
