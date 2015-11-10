d02e81cd522ea75de2994e0f0080c58fdfc84abe
zach char <zcharnell@gmail.com>
2015-11-09 21:19:49 -0700

    changed server.js

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-'use strict';
-
 var express = require('express'),
-	app = express();
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  bodyParser = require('body-parser'),
+  errorHandler = require('errorhandler'),
+  methodOverride = require('method-override');
 
-app.use('/js', express.static(__dirname + '/prod/js'));
-app.use('/sass', express.static(__dirname + '/dev/sass'));
-app.use('/css', express.static(__dirname + '/prod/css'));
-app.use('/templates', express.static(__dirname + '/dev/templates'));
-app.use('/json', express.static(__dirname + '/dev/json'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
-app.use('/assets', express.static(__dirname + '/prod/assets'));
-app.use('/images', express.static(__dirname + '/prod/assets/images'));
-
-app.get('/',function(req,res){
-	res.sendFile('index.html',{'root':__dirname + '/prod'});
-});
-
-app.listen('5000', function(){
-	console.log('Server running on port 5000');
-});
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/prod'));
