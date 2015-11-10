b00e39037bfc73b17a0a4f444c7d60feb3dee6b6
zach char <zcharnell@gmail.com>
2015-11-09 21:38:36 -0700

    updates

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
-  io = require('socket.io').listen(server),
   bodyParser = require('body-parser'),
   errorHandler = require('errorhandler'),
   methodOverride = require('method-override');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/prod'));
+
+server.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
